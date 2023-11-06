f2e9e6a6f1b3457b5a121bd3d75fafe4a6053227
RainbowBunny <vmc15566@gmail.com>
2023-11-06 21:47:16 +0700

    Added module word, module datastructure and dictionary need modification.

---
+++ src/main/java/model/datastructure/RadixTree.java
@@ -0,0 +1,71 @@
+package model.datastructure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RadixTree {
+    private final RadixTreeNode root;
+
+    /**
+     * Constructor for the radix tree, creating a new root.
+     */
+    public RadixTree() {
+        root = new RadixTreeNode(null);
+    }
+
+    /**
+     * This function is used to add a new word to the tree. Please note that the added word need to
+     * be in binary representation.
+     *
+     * @param word the added word in binary representation
+     */
+    public void add(int[] word) {
+        RadixTreeNode currentNode = root;
+        for (int i = 0; i < word.length; i++) {
+            if (currentNode.child[word[i]] == null) {
+                currentNode.child[word[i]] = new RadixTreeNode(currentNode);
+            }
+            currentNode = currentNode.child[i];
+        }
+        currentNode.setHasString(true);
+    }
+
+    /**
+     * This function is used to check if the word is present in the tree.
+     *
+     * @param word the word we want to check in binary representation
+     * @return true if the word in the tree
+     */
+    public boolean contain(int[] word) {
+        RadixTreeNode currentNode = root;
+        for (int i = 0; i < word.length; i++) {
+            if (currentNode.child[word[i]] == null) {
+                return false;
+            }
+            currentNode = currentNode.child[i];
+        }
+        return currentNode.isHasString();
+    }
+
+    /**
+     * This function will return best match word that match the prefix in lexico order.
+     *
+     * @param prefix the prefix we want to match
+     * @param numberOfNeededWord the number of needed word
+     * @return numberOfNeededWord word that match the prefix in the ascending lexico order
+     */
+    public List<Integer[]> findTenBestMatchWord(int[] prefix, int numberOfNeededWord) {
+        List<Integer[]> ans = new ArrayList<>();
+        RadixTreeNode currentNode = root;
+        for (int i = 0; i < prefix.length; i++) {
+            // Case the prefix does not exist in the tree
+            if (currentNode.child[prefix[i]] == null) {
+                return ans;
+            }
+            currentNode = currentNode.child[word[i]];
+        }
+
+
+        return ans;
+    }
+}
---
+++ src/main/java/model/word/Phonetic.java
@@ -0,0 +1,28 @@
+package model.word;
+
+public class Phonetic {
+    /**
+     * Potato
+     */
+    private final String text;
+    private final String audio;
+
+    /**
+     * Public constructors with text and parameters.
+     *
+     * @param text text representation of the phonetic
+     * @param audio link of audio file of the phonetic
+     */
+    Phonetic(String text, String audio) {
+        this.text = text;
+        this.audio = audio;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAudio() {
+        return audio;
+    }
+}
