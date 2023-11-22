d084bd8ac00605943243fbc998479e902f1f98c6
RainbowBunny <vmc15566@gmail.com>
2023-11-22 23:48:14 +0700

    Change icon, add file db, change app name

--- src/main/java/controller/MyApplication.java
+++ src/main/java/controller/MyApplication.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,8 +14,10 @@ public class MyApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MyApplication.class.getResource("/view/main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1266, 800);
-        stage.setTitle("Hello!");
+        stage.setTitle("D1CTATORSZ");
         stage.setScene(scene);
+        Image image = new Image(MyApplication.class.getResourceAsStream("/assets/SideMenu/Logo.png"));
+        stage.getIcons().add(image);
         stage.show();
     }
 
